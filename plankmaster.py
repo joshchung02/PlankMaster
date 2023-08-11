10f41ae12fcdbcfc7050116124a0cfb954145430
joshchung02 <joshchung02@gmail.com>
2023-08-11 13:06:18 -0700

    Main functionality done

--- plankmaster.py
+++ plankmaster.py
@@ -32,12 +32,33 @@ def main():
     breakLength.append(int(tmp2[0]))
     breakLength.append(int(tmp2[1]))
 
-    # Testing:
-    print(numSets)
-    print(setLength[0], setLength[1])
-    print(breakLength[0], breakLength[1])
+    # Print output banner
+    print()
+    print('+----------------------------------------------------------+')
+    print('|-------------------Here are your times--------------------|')
+    print('+----------------------------------------------------------+')
+    print()
 
     # Create output
+    currTime = [0, 0]
+
+    for s in range(numSets):
+        # Set
+        addTime(currTime, setLength)
+        
+        if currTime[1] > 9:
+            print(f"Set {s+1}:    {currTime[0]}:{currTime[1]}")
+        else:
+            print(f"Set {s+1}:    {currTime[0]}:0{currTime[1]}")
+        
+        addTime(currTime, breakLength)
+
+        # Break
+        if s == numSets - 1: break # Fencepost
+        if currTime[1] > 9:
+            print(f"Break {s+1}:  {currTime[0]}:{currTime[1]}")
+        else:
+            print(f"Break {s+1}:  {currTime[0]}:0{currTime[1]}")
 
 def validateTime(time):
     if len(time) != 4:
@@ -45,6 +66,17 @@ def validateTime(time):
     if time[1] != ':':
         return False
     return True
+
+def addTime(time, timeToAdd):
+    res = time
+    res[0] += timeToAdd[0]
+    res[1] += timeToAdd[1]
+
+    if res[1] > 59:
+        res[0] += 1
+        res[1] -= 60
+    
+    return res
 
 if __name__=='__main__':
     main()
